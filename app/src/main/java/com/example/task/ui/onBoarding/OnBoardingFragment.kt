ac71ef905bc6dd98368f5cb30a73805bcf78b0e7
itishn1k <kayrat1224mamataev@gmail.com>
2022-12-15 21:23:11 +0600

    начало

---
+++ app/src/main/java/com/example/task/ui/onBoarding/OnBoardingFragment.kt
@@ -0,0 +1,23 @@
+package com.example.task.ui.onBoarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.task.R
+import com.example.task.databinding.FragmentOnBoardingBinding
+
+class OnBoardingFragment : Fragment() {
+
+    private lateinit var binding: FragmentOnBoardingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentOnBoardingBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/example/task/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/task/ui/profile/ProfileFragment.kt
@@ -8,7 +8,6 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.task.databinding.FragmentProfileBinding
